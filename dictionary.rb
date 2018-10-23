eb316e18c1a82eed3b0e41bd657424ea577b0125
MD MAMUNUR RASHID <netmamun@yahoo.com>
2018-10-22 20:24:36 -0400

    dictionary class

--- dictionary.rb
+++ dictionary.rb
@@ -1,83 +1,54 @@
 class Dictionary
-  @@word_list = {}
-  def initialize()
-    @new_word = {}
 
-  end
-  def add (new_word)
-    if new_word.class == String
-      @new_word = { :"#{new_word}" => nil }
-    else
-      @new_word = new_word
+    def initialize
+      @entries = {}
+      @keywords = []
     end
 
-     @@word_list =  @new_word.merge(@@word_list)
-
-    # options = defaults.merge(options)
-  end
-
-  def entries
-      if @new_word.values
-        return @new_word
-      else
-        return @new_word.keys
-      end
-  end
+    def entries
+      @entries
+    end
 
     def keywords
+      @keywords.sort
+    end
 
-      return @new_word.keys
+    def entries=()
+      @entries
+    end
+
+    def keywords=()
+      @keywords
+    end
+
+    def add(entry)
+      if entry == entry.to_s
+        entry = {entry => nil}
+      end
+      entry.each do |k,v|
+        @keywords << k
+        @entries[k] = v
+      end
+
     end
 
 
-    def find (find_item)
-
-      @@word_list.each do |key, value|
-        if key == find_item
+    def include?(find_key)
+      if @keywords.empty?
+        return false
+      end
+      @keywords.each do |key|
+        if key == find_key
           return true
         else
           return false
         end
       end
-
     end
-  #
-  #
 
-
-
-  # def add()
-  #   return @new_word
-  # end
-
-  # def show_words
-  #   @@word_list.each do |key, value|
-  #     puts "#{key}..#{value}"
-  #
-  #   end
-
-
-
-  #
-  def include?(test_word)
-
-    @@word_list.each do |key, value|
-      if key == test_word
-        return true
-      else
-        return false
-      end
+    def find(item_find)
+      @entries.select{|k,v| k.start_with? item_find}
     end
+
+
   end
-
-end
-# my_word1 = Dictionary.new
-# my_word1.add = ({'fish' => 'aquatic animal'})
-#
-# my_word2 = Dictionary.new
-# my_word2.add = ({'crab' => 'aquatic animal'})
-#
-# # puts "#{my_word.add}"
-#
-# puts "#{my_word1.show_words}"
-# puts Dictionary.show_words
